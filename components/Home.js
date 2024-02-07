4df27d4d85d9705f1134130dfa20e5f137c86630
RELTIEN <pierre.reltien@gmail.com>
2024-02-07 14:18:33 +0100

    test

--- components/Home.js
+++ components/Home.js
@@ -12,7 +12,7 @@ function Home() {
 
   useEffect(() => {
 
-    fetch('http://localhost:3000/movies')
+    fetch('https://my-moviz-backend-kappa.vercel.app/movies')
       .then(response => response.json())
       .then((data) => {
         // console.log(data);
